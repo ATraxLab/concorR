15c92fe63c2559e79e13cfc925f2696949288903
Zeldoxsis <suda.4@wright.edu>
2020-03-30 19:22:55 -0400

    most . go poof, more spaces

--- R/CONCOR_blockmodeling.R
+++ R/CONCOR_blockmodeling.R
@@ -1,13 +1,13 @@
 
-make_blk <- function(adj.list, splitn = 1) {
+make_blk <- function(adj_list, splitn = 1) {
   #returns raw blockmodel data
 
   #run concor to get igraph splits, and shhh
-  con.out <- suppressWarnings(concor(adj.list, p = splitn))
+  concor_out <- suppressWarnings(concor(adj_list, p = splitn))
 
   #check and reorder the concor output to be the same as the vertex list
-  o <- match(colnames(adj.list[[1]]), con.out$vertex)
-  o.block <- con.out$block[o]
+  concor_order <- match(colnames(adj_list[[1]]), concor_out$vertex)
+  block_ordered <- concor_out$block[concor_order]
 
   ##prepare to attach sna (needed for next part) so it can later be detached easily
   #record the packages that existed at the start
@@ -21,7 +21,7 @@ make_blk <- function(adj.list, splitn = 1) {
   suppressMessages(library(sna))
 
   #make blokmodels (aka the thing all this has lead to)
-  blk.mod.list <- lapply(adj.list, function(x) blockmodel(as.matrix(x), o.block))
+  blockmodel_list <- lapply(adj_list, function(x) blockmodel(as.matrix(x), block_ordered))
 
   ##detach sna and reattach whatever else
   suppressMessages(invisible(lapply(paste('package:', names(sessionInfo()$otherPkgs), sep = ""),
@@ -29,64 +29,64 @@ make_blk <- function(adj.list, splitn = 1) {
   suppressMessages(invisible(lapply(pre, library, character.only = TRUE)))
 
   #return raw blockmodel data
-  return(blk.mod.list)
+  return(blockmodel_list)
 }
 
-edge.dens <- function(adj.mat) {
-  adj.mat[adj.mat>0] <- 1
-  a <- sum(adj.mat)
-  m <- length(adj.mat)-sqrt(length(adj.mat))
-  d <- a/m
+edge_dens <- function(adj_mat) {
+  adj_mat[adj_mat > 0] <- 1
+  a <- sum(adj_mat)
+  m <- length(adj_mat) - sqrt(length(adj_mat))
+  d <- a / m
   return(d)
 }
 
-make.red <- function(adj.list, splitn = 1, weighted = FALSE) {
-  #returns list of reduced matrixes one for each relation in $red.mat
+make_reduced <- function(adj_list, splitn = 1, weighted = FALSE) {
+  #returns list of reduced matrixes one for each relation in $red_mat
   #also returns the cutoff densities in $dens
   #inputs are list of adj matrixes what split you want and weather weighted or not (True is weighted)
 
   #get the raw blockmodel results
-  blk.out = make_blk(adj.list, splitn)
+  blk_out = make_blk(adj_list, splitn)
 
   #check density of the adjacency matrix
-  dens.vec <- sapply(adj.list, function(x) edge.dens(x))
+  dens_vec <- sapply(adj_list, function(x) edge_dens(x))
 
-  #pull out the density matrix of blk.out
-  d <- lapply(blk.out, function(x) x[[5]])
+  #pull out the density matrix of blk_out
+  d <- lapply(blk_out, function(x) x[[5]])
 
   #play with the matrix for later use
-  mat.return <- vector("list", length = length(dens.vec))
+  mat_return <- vector("list", length = length(dens_vec))
 
-  for (i in 1:length(dens.vec)) {
+  for (i in 1:length(dens_vec)) {
     temp1 <- d[[i]]
     #check for and make NaN to 0
     temp1[is.nan(temp1)] <- 0
     #set blocks with density less than overall density to 0
-    temp1[temp1<dens.vec[[i]]] <- 0
+    temp1[temp1 < dens_vec[[i]]] <- 0
     if (!weighted) {
-      temp1[temp1>0] <- 1
-      mat.return[[i]] <- temp1
+      temp1[temp1 > 0] <- 1
+      mat_return[[i]] <- temp1
     }
     if (weighted) {
       #find minimum density and scale all by that
-      min <- min(temp1[temp1>0])
-      mat.return[[i]] <- as.matrix(temp1/min)
+      min <- min(temp1[temp1 > 0])
+      mat_return[[i]] <- as.matrix(temp1 / min)
       #scale all densities to 10 or less
-      while (max(mat.return[[i]])>20) {
-        mat.return[[i]] <- mat.return[[i]]/1.05
+      while (max(mat_return[[i]]) > 20) {
+        mat_return[[i]] <- mat_return[[i]] / 1.05
       }
-      while (max(mat.return[[i]])<18) {
-        mat.return[[i]] <- mat.return[[i]]*1.05
+      while (max(mat_return[[i]]) < 18) {
+        mat_return[[i]] <- mat_return[[i]] * 1.05
       }
     }
   }
-  l.return <- list()
-  l.return$red.mat <- mat.return
-  l.return$dens <- dens.vec
-  return(l.return)
+  return_list <- list()
+  return_list$reduced_mat <- mat_return
+  return_list$dens <- dens_vec
+  return(return_list)
 }
 
-plot.blk <- function (x, ...) {
+plot_blk <- function (x, ...) {
   #slightly edited version of the function from SNA, plots as square and slightly changed labeling also attached SNA and later removed
   #why is it convention to not have comments
 
@@ -130,32 +130,32 @@ plot.blk <- function (x, ...) {
   suppressMessages(invisible(lapply(pre, library, character.only = TRUE)))
 }
 
-plot.blk.labeless <- function(bm) {
+plot_blk_labeless <- function(bm) {
   #get rid of those pesky labels
   bm$plabels <- rep("", length(bm$plabels))
   bm$glabels <- ""
-  plot.blk(bm)
+  plot_blk(bm)
 }
 
 
 #Igraph needed beyond this point
-make.red.igraph <- function(red.mat) {
+make_reduced_igraph <- function(reduced_mat) {
   w <- NULL
-  if (any(red.mat>1)) {
+  if (any(reduced_mat>1)) {
     w <- TRUE
   }
-  iplotty <- graph_from_adjacency_matrix(red.mat, mode = "directed", weighted = w)
+  iplotty <- graph_from_adjacency_matrix(reduced_mat, mode = "directed", weighted = w)
   return(iplotty)
 }
 
-plot.red.weighted <- function(blk) {
+plot_red_weighted <- function(blk) {
   #plots just weighted blockmodel network
   #plots colors based on order of blocks (I think this should match those on the networks)
   plot(blk, vertex.color = c(1:length(vertex.attributes(blk)[[1]])), vertex.label = NA,
        edge.width = (E(blk)$weight/3), edge.arrow.size = (E(blk)$weight/15), vertex.size = 25)
 }
 
-plot.red.unweighted <- function(blk) {
+plot_red_unweighted <- function(blk) {
   #plots just weighted blockmodel network
   #plots colors based on order of blocks (I think this should match those on the networks)
   plot(blk, vertex.color = c(1:length(vertex.attributes(blk)[[1]])), vertex.label = NA,
